51e1a5ce866999efcb4a0669009e0d79809e2e3f
夏悸 <wmails@126.com>
2025-12-10 11:20:40 +0800

    chore(release): 更新版本号到 2.4.0 并调整发布脚本
    - 将 package.json 中版本号从 2.3.9 更新为 2.4.0
    - 注释掉发布脚本中对 alpha、beta 和 patch 版本的自动处理逻辑
    - 修改 npm 发布命令，显式指定 npm 官方注册表地址
    - 保持其他发布头信息处理命令不变

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -16,15 +16,15 @@ git add themes
 set -e
 echo "修正版本号"
 
-if [ "$1" = '-alpha' ] ; then
-standard-version --prerelease alpha
-fi
-if [ "$1" = '-beta' ] ; then
-standard-version --prerelease beta
-fi
-if [ "$1" = '' ] ; then
-standard-version --release-as patch
-fi
+#if [ "$1" = '-alpha' ] ; then
+#standard-version --prerelease alpha
+#fi
+#if [ "$1" = '-beta' ] ; then
+#standard-version --prerelease beta
+#fi
+#if [ "$1" = '' ] ; then
+#standard-version --release-as patch
+#fi
 
 headr ./dist/heyui.js -o=./dist/heyui.js --version --homepage --copyright --license
 headr ./dist/heyui.esm.js -o=./dist/heyui.esm.js --version --homepage --copyright --license
@@ -38,7 +38,7 @@ fi
 echo "发布至npm"
 
  if [ "$1" = '' ] ; then
- npm publish
+ npm publish --registry=https://registry.npmjs.org/
  fi
 #if [ "$1" = '-alpha' ] ; then
 #npm publish --tag=next
